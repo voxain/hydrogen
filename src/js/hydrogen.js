fbd62ac83a3a9e9233f46a1fc187bcd4b4dfb381
rmcproductions <info@rmcprod.me>
2020-09-02 16:45:52 +0200

    commiting unfinished bullshit dont mind me

--- src/js/hydrogen.js
+++ src/js/hydrogen.js
@@ -1,4 +1,52 @@
 const electron = require('electron').remote.getCurrentWindow();
+
+let generateTab = url => {
+    let parser = new DOMParser();
+
+    let view = document.createElement('webview');
+    let tab = parser.parseFromString(`
+        <div class="sidebar-entry">
+            <div class="entry-icon">
+                ::ICON::
+            </div>
+            <div class="entry-tooltip">
+                ::NAME::
+            </div>
+        </div>
+    `.replace(/::NAME::/g, url)
+    .replace(/::ICON::/g, '<div class="lds-ripple"><div></div><div></div></div>')
+    , 'text/html');
+
+    return {
+        tab,
+        view
+    };
+}
+
+class tab{
+    constructor(initURL, id){
+        this.id = id || Math.round( Math.random() * 0x10000 );
+        this.DOM = generateTab(initURL);
+        
+        console.log(this.DOM.tab.body.childNodes)
+        document.querySelector('#tablist').append(this.DOM.tab.body);
+    }
+
+    loadURL(url){
+        // Set Tab DOM to display name and icon correctly
+        // Set webview to load the tab (also catch loading event)
+    }
+}
+
+let hydrogen = {
+    tabs: {
+        list: [],
+        current: '1'
+    }
+}
+
+
+
 function loaded() {
     document.querySelector('#addressbar-input').addEventListener('keypress', e => {
         if(e.which == 13) {
